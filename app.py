76a9982e232f420b96b54aa8ac6781a8f3dd52f3
Christopher <bryanignacio99@gmail.com>
2020-10-12 23:38:22 -0600

    metodo modificar y eliminar
    agregados

--- app.py
+++ app.py
@@ -30,5 +30,27 @@ def agregarPersona():
 def obtenerPersonas():
     return jsonify(personas)
 
+@app.route('/modificar',methods=['POST'])
+def modificarPersona():
+    cuerpo = request.get_json()
+    nombre = cuerpo['nombre']
+    new_nombre = cuerpo['new_nombre']
+    new_edad = cuerpo['new_edad']
+    global personas
+    for i in range(len(personas)):
+        if personas[i]['nombre'] == nombre:
+            personas[i]['nombre'] = new_nombre
+            personas[i]['edad'] = new_edad
+    return jsonify({"mensaje":"Modificado correctamente"})
+
+@app.route('/eliminar',methods=['POST'])
+def eliminarPersona():
+    cuerpo = request.get_json()
+    nombre = cuerpo['nombre']
+    global personas
+    for i in range(len(personas)):
+        if personas[i]['nombre'] == nombre:
+            personas.pop(i)
+    return jsonify({"mensaje":"Eliminado correctamente"})  
 if __name__ == '__main__':
-    app.run(debug=True,port=4000)
+    app.run(debug=True,host='0.0.0.0',port=4000)
